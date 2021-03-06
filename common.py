bcf671195188a1ad83051b420583feef0793f709
mellik21 <korolevazaek@gmail.com>
2021-03-06 15:34:30 +0400

    parser done

--- common.py
+++ common.py
@@ -5,3 +5,13 @@ import pytz
 def get_now_formatted() -> str:
     now = datetime.datetime.now(pytz.timezone("Europe/Moscow")).strftime("%Y-%m-%d %H:%M:%S")
     return now.strftime("%Y-%m-%d %H:%M:%S")
+
+
+def appendTextOrAttr(value, list, attr=None):
+    if value is not None:
+        if attr is not None:
+            list.append(value[attr])
+        else:
+            list.append(value.text)
+    else:
+        list.append("")
--- parser.py
+++ parser.py
@@ -2,58 +2,64 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 import pandas as pd
 import constantKeeper as keeper
+import common
+
+'''Парсинг сайта и формирование anime.csv'''
 
 driver = webdriver.Chrome(keeper.CHROME_DRIVER_PATH)
-rus_names, eng_names, hrefs, genres, ratings, descriptions, alt_descriptions, studios, minor_names = ([] for _ in
-                                                                                                      range(9))
+rus_names, eng_names, hrefs, genres, ratings, descriptions, alt_descriptions, studios, minor_names, img_paths = \
+    ([] for _ in range(10))
 
-driver.get("https://smotret-anime.online/anime?page=1")
-content = driver.page_source
-soup = BeautifulSoup(content, "html.parser")
+PAGES_NUM = 633
+for i in (0, PAGES_NUM):
+    driver.get("https://smotret-anime.online/anime?page=" + str(i))
+    parser = BeautifulSoup(driver.page_source, "html.parser")
 
-for anime_div in soup.findAll('div', attrs={'class': 'col s12 m4 l3'}):
-    eng_name = anime_div.find('h4', attrs={'class': 'line-2'})
-    rus_name = anime_div.find('h5', attrs={'class': 'line-1'})
-    a = rus_name.find('a', href=True)
-    href = a['href']
+    '''Перебор всех фильмов на текущей странице'''
+    for anime_div in parser.findAll('div', attrs={'class': 'col s12 m4 l3'}):
+        eng_name = anime_div.find('h4', attrs={'class': 'line-2'})
+        common.appendTextOrAttr(eng_name, eng_names)
 
-    driver.get("https://smotret-anime.online" + href)
-    c = driver.page_source
-    s = BeautifulSoup(c, "html.parser")
-    local_genres = []
-    for genre_a in s.findAll('a', attrs={'class': 'm-genres-list__item'}):
-        local_genres.append(genre_a.text)
+        rus_name = anime_div.find('h5', attrs={'class': 'line-1'})
+        common.appendTextOrAttr(rus_name, rus_names)
 
-    local_descriptions = []
-    for desc_div in s.findAll('div', attrs={'class': 'm-description-item'}):
-        desc = desc_div.find('div', attrs={'class': 'card-content'})
-        local_descriptions.append(desc.text)
+        a = rus_name.find('a', href=True)
+        href = a['href']
+        hrefs.append(href)
 
-    local_studios = []
-    for studio_a in s.findAll('a', attrs={'class': 'm-studios-list_item'}):
-        local_studios.append(studio_a['href'])
+        '''Переход на страницу фильма для выбора более полной информации'''
+        driver.get("https://smotret-anime.online" + href)
+        p = BeautifulSoup(driver.page_source, "html.parser")
+        local_genres, local_descriptions, local_studios = ([] for _ in range(3))
+        for genre_a in p.findAll('a', attrs={'class': 'm-genres-list__item'}):
+            local_genres.append(genre_a.text)
+        genres.append(local_genres)
 
-    local_minor_names = s.find('div', attrs={'class': 'm-minor-titles-list'})
+        for desc_div in p.findAll('div', attrs={'class': 'm-description-item'}):
+            desc = desc_div.find('div', attrs={'class': 'card-content'})
+            local_descriptions.append(desc.text)
+        descriptions.append(local_descriptions)
 
-    if local_minor_names is not None:
-        minor_names.append(local_minor_names.text)
-    else:
-        minor_names.append("")
+        for studio_a in p.findAll('a', attrs={'class': 'm-studios-list_item'}):
+            local_studios.append(studio_a['href'])
+        studios.append(local_studios)
 
-    studios.append(local_studios)
-    descriptions.append(local_descriptions)
-    genres.append(local_genres)
-    hrefs.append(href)
-    rus_names.append(rus_name.text)
-    eng_names.append(eng_name.text)
-print(len(rus_names), len(eng_names), len(hrefs), len(genres), len(descriptions), len(studios), len(minor_names))
+        local_minor_names = p.find('div', attrs={'class': 'm-minor-titles-list'})
+        common.appendTextOrAttr(local_minor_names, minor_names)
+
+        rating = p.find('span', attrs={'itemprop': 'ratingValue'})
+        common.appendTextOrAttr(rating, ratings)
+
+        img = p.find('img', attrs={'itemprop': 'contentUrl'})
+        common.appendTextOrAttr(img, img_paths, 'src')
+
 df = pd.DataFrame({'Rus_name': rus_names,
                    'Eng_name': eng_names,
                    'Href': hrefs,
                    'Genres': genres,
-                   #     'Rating': ratings,
+                   'Rating': ratings,
                    'Description': descriptions,
-                   #   'Alt_desc': alt_descriptions,
+                   'Imgs': img_paths,
                    'Studios': studios,
                    'Minor_names': minor_names})
 df.to_csv('anime.csv', index=False, encoding='utf-8')

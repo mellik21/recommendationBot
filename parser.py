ba3bfee75ef3cec39041a4ec673a740620f3d41c
Anastasia <52373473+mellik21@users.noreply.github.com>
2021-03-10 17:54:09 +0400

    Update parser.py

--- parser.py
+++ parser.py
@@ -42,49 +42,53 @@ def appendDf():
     previous_data = previous_data.append(df, ignore_index=True)
     previous_data.to_csv('anime.csv', index=False, encoding='utf-8')
 
-for i in range(1, 11):
-    driver.get("https://smotret-anime.online/anime?page=" + str(i))
-    parser = BeautifulSoup(driver.page_source, "html.parser")
+def parse(left, right):
+    for i in range(left, right):
+        driver.get("https://smotret-anime.online/anime?page=" + str(i))
+        parser = BeautifulSoup(driver.page_source, "html.parser")
 
-    '''Перебор всех фильмов на текущей странице'''
-    for anime_div in parser.findAll('div', attrs={'class': 'col s12 m4 l3'}):
-        eng_name = anime_div.find('h4', attrs={'class': 'line-2'})
-        common.appendTextOrAttr(eng_name, eng_names)
-        rus_name = anime_div.find('h5', attrs={'class': 'line-1'})
-        common.appendTextOrAttr(rus_name, rus_names)
+        '''Перебор всех фильмов на текущей странице'''
+        for anime_div in parser.findAll('div', attrs={'class': 'col s12 m4 l3'}):
+            eng_name = anime_div.find('h4', attrs={'class': 'line-2'})
+            common.appendTextOrAttr(eng_name, eng_names)
+            rus_name = anime_div.find('h5', attrs={'class': 'line-1'})
+            common.appendTextOrAttr(rus_name, rus_names)
 
-        a = rus_name.find('a', href=True)
-        href = a['href']
-        hrefs.append(href)
+            a = rus_name.find('a', href=True)
+            href = a['href']
+            hrefs.append(href)
 
-        '''Переход на страницу фильма для выбора более полной информации'''
-        driver.get("https://smotret-anime.online" + href)
-        pages.append(i)
-        p = BeautifulSoup(driver.page_source, "html.parser")
-        local_genres, local_descriptions, local_studios = ([] for _ in range(3))
-        for genre_a in p.findAll('a', attrs={'class': 'm-genres-list__item'}):
-            local_genres.append(genre_a.text)
-        genres.append(local_genres)
+            '''Переход на страницу фильма для выбора более полной информации'''
+            driver.get("https://smotret-anime.online" + href)
+            pages.append(i)
+            p = BeautifulSoup(driver.page_source, "html.parser")
+            local_genres, local_descriptions, local_studios = ([] for _ in range(3))
+            for genre_a in p.findAll('a', attrs={'class': 'm-genres-list__item'}):
+                local_genres.append(genre_a.text)
+            genres.append(local_genres)
 
-        for desc_div in p.findAll('div', attrs={'class': 'm-description-item'}):
-            desc = desc_div.find('div', attrs={'class': 'card-content'})
-            local_descriptions.append(desc.text)
-        descriptions.append(local_descriptions)
+            for desc_div in p.findAll('div', attrs={'class': 'm-description-item'}):
+                desc = desc_div.find('div', attrs={'class': 'card-content'})
+                local_descriptions.append(desc.text)
+            descriptions.append(local_descriptions)
 
-        for studio_a in p.findAll('a', attrs={'class': 'm-studios-list_item'}):
-            local_studios.append(studio_a['href'])
-        studios.append(local_studios)
+            for studio_a in p.findAll('a', attrs={'class': 'm-studios-list_item'}):
+                local_studios.append(studio_a['href'])
+            studios.append(local_studios)
 
-        local_minor_names = p.find('div', attrs={'class': 'm-minor-titles-list'})
-        common.appendTextOrAttr(local_minor_names, minor_names)
+            local_minor_names = p.find('div', attrs={'class': 'm-minor-titles-list'})
+            common.appendTextOrAttr(local_minor_names, minor_names)
 
-        rating = p.find('span', attrs={'itemprop': 'ratingValue'})
-        common.appendTextOrAttr(rating, ratings)
+            rating = p.find('span', attrs={'itemprop': 'ratingValue'})
+            common.appendTextOrAttr(rating, ratings)
 
-        img = p.find('img', attrs={'itemprop': 'contentUrl'})
-        common.appendTextOrAttr(img, img_paths, 'src')
-        
-        addition = p.find('div', attrs={'class': 'card-content'}).find('p')
-        common.appendTextOrAttr(addition, additional)
+            img = p.find('img', attrs={'itemprop': 'contentUrl'})
+            common.appendTextOrAttr(img, img_paths, 'src')
 
-appendDf()
+            addition = p.find('div', attrs={'class': 'card-content'}).find('p')
+            common.appendTextOrAttr(addition, additional)
+
+    appendDf()
+    
+    
+parse(1,11)

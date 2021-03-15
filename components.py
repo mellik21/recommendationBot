b6b5f84371e9624eb13550f8a859c0a8451d4751
mellik21 <korolevazaek@gmail.com>
2021-03-15 23:00:35 +0400

    Added callback methods

--- components.py
+++ components.py
@@ -3,6 +3,7 @@ from telebot import types
 DEFAULT_MARKUP = None
 ASSESSMENT_MARKUP = None
 FILM_MARKUP = None
+EMPTY_MARKUP = None
 
 
 def initGlobalMarkup():
@@ -15,20 +16,25 @@ def initGlobalMarkup():
 def initAssessmentMarkup():
     global ASSESSMENT_MARKUP
     ASSESSMENT_MARKUP = types.InlineKeyboardMarkup()
-    ASSESSMENT_MARKUP.add(types.InlineKeyboardButton('Да', callback_data='yes'))
-    ASSESSMENT_MARKUP.add(types.InlineKeyboardButton('Нет', callback_data='no'))
+    ASSESSMENT_MARKUP.add(types.InlineKeyboardButton('Да', callback_data='like'))
+    ASSESSMENT_MARKUP.add(types.InlineKeyboardButton('Нет', callback_data='not_like'))
 
 
 def initFilmMarkup():
     global FILM_MARKUP
     FILM_MARKUP = types.InlineKeyboardMarkup()
-    FILM_MARKUP.add(types.InlineKeyboardButton('Информация', callback_data='info'))
-    FILM_MARKUP.add(types.InlineKeyboardButton('Нравится', callback_data='like'))
-    FILM_MARKUP.add(types.InlineKeyboardButton('Не нравится', callback_data='not_like'))
-    FILM_MARKUP.add(types.InlineKeyboardButton('Уже смотрел', callback_data='seen'))
+    FILM_MARKUP.add(types.InlineKeyboardButton('Возможно позже', callback_data='like'))
+    FILM_MARKUP.add(types.InlineKeyboardButton('Точно не буду смотреть', callback_data='not_like'))
+    FILM_MARKUP.add(types.InlineKeyboardButton('Уже видел', callback_data='seen'))
+
+
+def initEmptyMarkup():
+    global EMPTY_MARKUP
+    EMPTY_MARKUP = types.InlineKeyboardMarkup()
 
 
 def initDefaultComponents():
     initGlobalMarkup()
     initAssessmentMarkup()
     initFilmMarkup()
+    initEmptyMarkup()
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import telebot
 import config as keeper
 from telebot import types
 import components
+from PIL import Image
 
 bot = telebot.TeleBot(keeper.API_TOKEN)
 defaultMarkup = None
@@ -18,17 +19,56 @@ def welcome(message):
 def text_handler(message):
     if message.chat.type == 'private':
         if message.text == 'Что посмотреть?':
-            bot.send_message(message.chat.id, "")
+            img = open('files/test.jpg', 'rb')
+            bot.send_photo(message.chat.id, img, caption='Представьте что рекомендация работает 1 сезон \n⭐ '
+                                                         'рейтинг: 9.2 \n Крыс Реми обладает уникальным вкусом. '
+                                                         'Он готов рисковать собственной жизнью, чтобы '
+                                                         'посмотреть любимое кулинарное шоу и раздобыть '
+                                                         'какую-нибудь приправку или просто свежий продукт. '
+                                                         'Реми живет со своими сородичами, которые его не '
+                                                         'понимают и не принимают его увлечения кулинарией. '
+                                                         'Когда Реми случайно попадает на кухню шикарного '
+                                                         'ресторана, он решает воспользоваться выпавшим ему '
+                                                         'шансом и проверить свои навыки. '
+                                                         '', reply_markup=components.FILM_MARKUP)
+
         elif message.text == 'Уточнить мои интересы':
-            bot.register_next_step_handler(bot.send_message(message.chat.id,
-                                                            "Введите название любимого аниме. "
-                                                            "Мы исключим его из рекомендаций и скорректируем ваши предпочтения!"),
-                                           search)
+            bot.register_next_step_handler(
+                bot.send_message(message.chat.id,
+                                 "Введите название любимого аниме. "
+                                 "Мы исключим его из рекомендаций "
+                                 "и скорректируем ваши предпочтения!"), search)
 
 
 def search(message):
     bot.send_message(message.chat.id, "Закройте глаза и представьте что поиск работает. Мы угадали?",
                      reply_markup=components.ASSESSMENT_MARKUP)
+
+
+@bot.callback_query_handler(func=lambda call: call.data in ['like', 'not_like'])
+def callback_inline_assessments(call):
+    message = call.message
+    if message:
+        #   if call.data == 'like':
+        # else call.data == 'not_like':
+        if message.caption is not None:
+            bot.edit_message_caption(chat_id=message.chat.id, message_id=message.message_id, caption=message.caption,
+                                     reply_markup=components.EMPTY_MARKUP)
+        else:
+            bot.edit_message_text(chat_id=message.chat.id, message_id=message.message_id, text=message.text,
+                                  reply_markup=components.EMPTY_MARKUP)
+
+        bot.send_message(message.chat.id, "Обязательно учтем это!")
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline_assessments(call):
+    message = call.message
+    if message:
+        if call.data == 'seen':
+            bot.edit_message_caption(chat_id=message.chat.id, message_id=message.message_id, caption=message.caption,
+                                     reply_markup=components.EMPTY_MARKUP)
+            bot.send_message(message.chat.id, "Ого, здорово! Понравилось?", reply_markup=components.ASSESSMENT_MARKUP)
 
 
 components.initDefaultComponents()
